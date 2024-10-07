4333924ad7fd56f747ff5410537839c23ebb0e59
JustDevRae <ksrae165@gmail.com>
2024-10-07 20:51:19 +0900

    refactor: using useCallback hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import { useRef, useReducer } from "react"
+import { useRef, useReducer, useCallback } from "react"
 import Editor from "./components/Editor"
 import Header from "./components/Header"
 import List from "./components/List"
@@ -42,7 +42,7 @@ function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
-  const onCreate = (content) => {
+  const onCreate = useCallback((content) => {
     dispatch({
       type: "CREATE",
       data: {
@@ -52,21 +52,21 @@ function App() {
         date: new Date().getTime(),
       }
     })
-  }
+  }, [])
 
-  const onUpdate = (targetId) => {
+  const onUpdate = useCallback((targetId) => {
     dispatch({
       type: "UPDATE",
       targetId: targetId,
     })
-  }
+  }, [])
 
-  const onDelete = (targetId) => {
+  const onDelete = useCallback((targetId) => {
     dispatch({
       type: "DELETE",
       targetId: targetId,
     })
-  }
+  }, [])
 
   return (
     <div className="App">
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -19,14 +19,4 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   </div>
 }
 
-export default memo(TodoItem, (prevProps, nextProps) => {
-  // 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단
-  // true -> Props가 바뀌지 않음
-  // false -> Props가 바뀜
-  if (prevProps.id !== nextProps.id) return false;
-  if (prevProps.isDone !== nextProps.isDone) return false;
-  if (prevProps.content !== nextProps.content) return false;
-  if (prevProps.date !== nextProps.date) return false;
-
-  return true;
-});
+export default memo(TodoItem);
